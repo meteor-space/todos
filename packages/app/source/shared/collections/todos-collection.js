839d619d1cf7724d392a63dd779fa14aea7a953c
Darko Mijić <darko.mijic@ebiz.hr>
2015-11-23 05:33:21 +0100

    Removes unnecessary allow rules

--- packages/app/source/shared/collections/todos-collection.js
+++ packages/app/source/shared/collections/todos-collection.js
@@ -8,11 +8,4 @@ todos.findActiveTodos = function() {
   return todos.find({ isCompleted: false });
 };
 
-// Contrived example! Here you could specify your real rules
-todos.allow({
-  insert() { return true; },
-  update() { return true; },
-  remove() { return true; }
-});
-
 Todos.Todos = todos;
