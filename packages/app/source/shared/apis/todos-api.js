9726c4c1edc9e1a0f5dda132ff2ed117b25b58cd
darkomijic <darko.mijic@gmail.com>
2015-11-25 16:15:59 +0100

    Adds methods for completing and reopening todo to the api

--- packages/app/source/shared/apis/todos-api.js
+++ packages/app/source/shared/apis/todos-api.js
@@ -8,12 +8,14 @@ Space.messaging.Api.extend(Todos, 'TodosApi', {
     return [{
       'Todos.CreateTodoList': this._createTodoList,
       'Todos.CreateTodo': this._createTodo,
+      'Todos.CompleteTodo': this._completeTodo,
+      'Todos.ReopenTodo': this._reopenTodo
     }];
   },
 
   _createTodoList(context, command) {
     if (context.isSimulation) {
-
+      // TODO
     } else {
       this.send(command);
     }
@@ -22,14 +24,29 @@ Space.messaging.Api.extend(Todos, 'TodosApi', {
   _createTodo(context, command) {
     if (context.isSimulation) {
       this.todos.insert({
-          listId: event.sourceId,
-          title: event.title,
-          isCompleted: event.isCompleted
-        }
-      );
+        listId: event.sourceId,
+        title: event.title,
+        isCompleted: event.isCompleted
+      });
     } else {
       this.send(command);
     }
   },
+
+  _completeTodo(context, command) {
+    if (context.isSimulation) {
+      // TODO
+    } else {
+      this.send(command);
+    }
+  },
+
+  _reopenTodo(context, command) {
+    if (context.isSimulation) {
+      // TODO
+    } else {
+      this.send(command);
+    }
+  }
 
 });
