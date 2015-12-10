6a4c0dcc3b844a97266f8112425bf2667a6fa334
Darko Mijić <darko.mijic@ebiz.hr>
2015-12-10 01:21:57 +0100

    Update to the latest TodoMVC html and css template

--- packages/app/source/client/views/input/input.js
+++ packages/app/source/client/views/input/input.js
@@ -6,22 +6,22 @@ Space.flux.BlazeComponent.extend(Todos, 'Input', {
 
   events() {
     return [{
-      'keyup #new-todo'(event) {
+      'keyup .new-todo'(event) {
 
         // When it was the ENTER key
         if (event.keyCode === 13) {
 
           // Tell mediator about it
-          var input = this.$('#new-todo').val();
+          var input = this.$('.new-todo').val();
           this.publish(new Todos.TodoCreated({
             title: input
           }));
 
           // Reset input
-          this.$('#new-todo').val('');
+          this.$('.new-todo').val('');
         }
       }
     }];
-  },
+  }
 
 });
--- packages/app/source/client/views/todo_list/components/todo.js
+++ packages/app/source/client/views/todo_list/components/todo.js
@@ -1,89 +1,89 @@
 Space.flux.BlazeComponent.extend(Todos, 'Todo', {
 
-    dependencies: {
-      store: 'Todos.TodosStore',
-      meteor: 'Meteor'
-    },
+  dependencies: {
+    store: 'Todos.TodosStore',
+    meteor: 'Meteor'
+  },
 
-    completedState() {
-      return this.isCompleted ? 'completed' : '';
-    },
+  completedState() {
+    return this.isCompleted ? 'completed' : '';
+  },
 
-    editingState() {
-      let template = Template.instance();
-      this.isEditing = this.store.editingTodoId() === template.data.id;
-      if (this.isEditing) {
-        if (template.view.isRendered) {
-          template.$('.edit').focus().select();
-        }
-        return 'editing';
+  editingState() {
+    let template = Template.instance();
+    this.isEditing = this.store.editingTodoId() === template.data.id;
+    if (this.isEditing) {
+      if (template.view.isRendered) {
+        template.$('.edit').focus().select();
       }
-      else {
-        return '';
-      }
-    },
-
-    events() {
-      return [{
-        'click .toggle': this._toggleTodo,
-        'click .destroy': this._removeTodo,
-        'dblclick .todo': this._editTodo,
-        'blur .edit': this._stopEditing,
-        'keyup .edit'(event) {
-          switch (event.keyCode) {
-            case 27: // escape key
-              this._stopEditing(event);
-              break;
-            case 13: // enter key
-              this._submitNewTitle(event);
-              break;
-          }
-        }
-      }];
-    },
-
-    _toggleTodo() {
-      if (this.currentData().isCompleted) {
-        this.publish(new Todos.TodoReopened({
-          todoId: this.currentData().id
-        }));
-      } else {
-        this.publish(new Todos.TodoCompleted({
-          todoId: this.currentData().id
-        }));
-      }
-    },
-
-    _removeTodo() {
-      this.publish(new Todos.TodoRemoved({
-        todoId: this.currentData().id
-      }));
-    },
-
-    _editTodo() {
-      this.publish(new Todos.TodoEditingStarted({
-        todoId: this.currentData().id
-      }));
-    },
-
-    _stopEditing() {
-      this.publish(new Todos.TodoEditingEnded({
-        todoId: this.currentData().id
-      }));
-    },
-
-    _submitNewTitle(event) {
-      let template = Template.instance();
-      let id = template.data.id;
-      let newTitle = event.currentTarget.value;
-      this.publish(new Todos.TodoTitleChanged({
-        todoId: id,
-        newTitle: newTitle
-      }));
-      this._stopEditing();
+      return 'editing';
     }
+    else {
+      return '';
+    }
+  },
 
-  })
-  // Register blaze-component for template
-  .register('todo');
+  events() {
+    return [{
+      'click .toggle': this._toggleTodo,
+      'click .destroy': this._removeTodo,
+      'dblclick .todo': this._editTodo,
+      'blur .edit': this._stopEditing,
+      'keyup .edit'(event) {
+        switch (event.keyCode) {
+          case 27: // escape key
+            this._stopEditing(event);
+            break;
+          case 13: // enter key
+            this._submitNewTitle(event);
+            break;
+        }
+      }
+    }];
+  },
+
+  _toggleTodo() {
+    if (this.currentData().isCompleted) {
+      this.publish(new Todos.TodoReopened({
+        todoId: this.currentData().id
+      }));
+    } else {
+      this.publish(new Todos.TodoCompleted({
+        todoId: this.currentData().id
+      }));
+    }
+  },
+
+  _removeTodo() {
+    this.publish(new Todos.TodoRemoved({
+      todoId: this.currentData().id
+    }));
+  },
+
+  _editTodo() {
+    this.publish(new Todos.TodoEditingStarted({
+      todoId: this.currentData().id
+    }));
+  },
+
+  _stopEditing() {
+    this.publish(new Todos.TodoEditingEnded({
+      todoId: this.currentData().id
+    }));
+  },
+
+  _submitNewTitle(event) {
+    let template = Template.instance();
+    let id = template.data.id;
+    let newTitle = event.currentTarget.value;
+    this.publish(new Todos.TodoTitleChanged({
+      todoId: id,
+      newTitle: newTitle
+    }));
+    this._stopEditing();
+  }
+
+})
+// Register blaze-component for template
+.register('todo');
 
